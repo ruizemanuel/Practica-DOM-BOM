dc799ad1c51c2731911536443cef45f0624c7a08
Emanuel Ruiz <ruiz.e.642@gmail.com>
2022-11-05 20:14:44 -0300

    se modifico la fecha en el punto 4

--- punto 4/js/app.js
+++ punto 4/js/app.js
@@ -2,7 +2,39 @@
 
 function reloj() {
     let date = new Date()
+    let dia = date.getDate()
+    let diaSemana = date.getDay()
+    ///////////////////Agregar dia de la semana y numero del mes/////////////////////////////
 
+    document.querySelector("#diaSemana").innerHTML = dia + " "
+    switch (diaSemana) {
+        case 1:
+            diaSemana = "Lunes"
+            break;
+        case 2:
+            diaSemana = "Martes"
+            break;
+        case 3:
+            diaSemana = "Miercoles"
+            break;
+        case 4:
+            diaSemana = "Jueves"
+            break;
+        case 5:
+            diaSemana = "Viernes"
+            break;
+        case 6:
+            diaSemana = "Sabado"
+            break;
+        case 7:
+            diaSemana = "Domingo"
+            break;
+
+    }
+    document.querySelector("#dia").innerHTML = diaSemana + " "
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////
     let horas = date.getHours()
     let minutos = date.getMinutes()
     let segundos = date.getSeconds()
@@ -25,7 +57,7 @@ function reloj() {
         ampm = "AM"
     }
 
-    
+
 
     document.querySelector("#ampm").innerHTML = ampm
     document.querySelector("#horas").innerHTML = horas
